9eda347d2bee631c12b540d42088647b14531da1
Rossello- Poulet Arthur <arthur.rossello-poulet@insa-lyon.fr>
2021-10-20 17:32:04 +0200

    Version finale avec les warnings

--- src/src/http/server/WebServer.java
+++ src/src/http/server/WebServer.java
@@ -22,7 +22,7 @@ public class WebServer {
             // create the main server socket
             s = new ServerSocket(3000);
         } catch (Exception e) {
-            System.out.println("Error: " + e);
+            e.printStackTrace();
             return;
         }
 
@@ -52,7 +52,6 @@ public class WebServer {
 
 
                 if (!header.isEmpty()) {
-                    System.out.println("on rentre dans le switch");
                     switch (methodRequested) {
                         case "GET":
                             executeGETmethod(resourceRequested, dataOut, out);
@@ -76,47 +75,36 @@ public class WebServer {
                 dataOut.flush();
                 socketClient.close();
             } catch (Exception e) {
-                System.out.println("Error: " + e);
+                e.printStackTrace();
             }
         }
     }
 
-
-    // donne le type du fichier en fonction de son extension
-    public String getTypeFromExtension(String extension) {
-        String type = "";
-        switch (extension) {
-            case "jpg":
-            case "jpeg":
-                type = "image/jpeg";
-                break;
-            case "gif":
-                type = "image/gif";
-                break;
-            case "png":
-                type = "image/png";
-                break;
-            case "mp4":
-                type = "video/mp4";
-                break;
-            case "docx":
-                type = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-                break;
-            case "pdf":
-                type = "application/pdf";
-                break;
-            case "md":
-                type = "text/markdown";
-                break;
-            case "html":
-                type = "text/html";
-                break;
-            default:
-                type = "text/plain";
-                break;
+    // lecture du header envoyé par le client
+    public List<String> getHeaderFromClient(BufferedInputStream in) throws IOException {
+        List<String> listeHeader = new ArrayList<>();
+        String header = "";
+        int lecteur = in.read();
+        // tant que la lecture du fichier n'est pas terminée
+        while (lecteur != -1) {
+            // on ajoute le caractère à la fin du strinBuilder
+            header += (char) lecteur;
+            // si la ligne est finie, on regarde si le fichier est fini ou s'il faut continuer la lecture
+            if (header.endsWith("\r\n")) {
+                if (header.equals("\r\n")) { // on a atteint la fin du header
+                    break;
+                } else {
+                    // on ajoute la ligne actuelle
+                    listeHeader.add(header.substring(0, header.lastIndexOf("\r\n")));
+                    header = "";
+                }
+            }
+            lecteur = in.read();
         }
-        return type;
+        System.out.println(listeHeader);
+        return listeHeader;
     }
+
 
     // execute la requete selon la methode get
     public void executeGETmethod(String fileName, BufferedOutputStream dataOut, PrintWriter out) throws IOException {
@@ -131,70 +119,9 @@ public class WebServer {
 
     // execute la requete selon la methode head
     public void executeHEADmethod(String fileName, BufferedOutputStream dataOut, PrintWriter out) throws IOException {
-
         buildHeaderGetandPost(fileName, out);
     }
 
-    // Construction du header pour les méthodes get et head. Renvoie le fichier à afficher dans le cas de la méthode get
-    public File buildHeaderGetandPost(String fileName, PrintWriter out) throws IOException {
-        // si pas de fichier spécifié, on affiche index.html
-        if (fileName.equals("/") || fileName.equals("")) {
-            fileName = INIT_DIR + "index.html";
-        } else {
-            fileName = INIT_DIR + fileName;
-        }
-        File file = new File(fileName);
-        String codeStatus = "OK 200", extension = "";
-        // on vérifie que le fichier existe, sinon message d'erreur
-        if (!file.exists()) {
-            codeStatus = "404 not found";
-            fileName = INIT_DIR + "error404.html";
-            file = new File(fileName);
-        }
-        int fileLength = (int) file.length();
-        extension = null;
-        int extensionPos = fileName.lastIndexOf('.');
-        if (extensionPos > 0) {
-            extension = fileName.substring(extensionPos + 1);
-        } else { // pas sûr de vraiment pouvoir passer là
-            codeStatus = "404 not found";
-        }
-
-        String content = getTypeFromExtension(extension);
-        // on affiche le headerr avec les infos récolotées
-        printHeader(content, codeStatus, fileLength, out, fileName);
-
-        return file;
-    }
-
-    // on lit le fichier en paramètre et on le retourne sous forme de tableau de byte
-    public byte[] readData(File file) throws IOException {
-        int lengthFile = (int) file.length();
-        FileInputStream dataStream = null;
-        byte[] dataArray = new byte[lengthFile];
-        try {
-            dataStream = new FileInputStream(file);
-            dataStream.read(dataArray);
-        } finally {
-            if (dataStream != null) dataStream.close();
-        }
-        return dataArray;
-    }
-
-    // affiche le header dans le print writer out
-    public void printHeader(String content, String status, int length, PrintWriter out, String fileName) throws IOException {
-        String header = "HTTP/1.1 " + status + "\r\n";
-        header += "Server: Java HTTP Server from Capucine and Arthur : 1.0" + "\r\n";
-        header += "Date: " + new Date() + "\r\n";
-        if (!content.equals(""))
-            header += "Content-Type: " + content + "\r\n";
-        if (length != -1)
-            header += "Content-length: " + length + "\r\n";
-        if (!fileName.equals(""))
-            header += "Content-name: " + fileName + "\r\n";
-        out.println(header);
-        out.flush();
-    }
 
     // Execution de la méthode post
     public void executePOSTmethod(BufferedInputStream in, BufferedOutputStream dataOut, String fileName, PrintWriter out) throws IOException {
@@ -266,41 +193,39 @@ public class WebServer {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-            ;
         }
     }
-    
-    // on écrit les données récupérées au format byte pour les insérer dans le fichier en paramètre
-    private void writeData(File file, byte[] fileData, boolean exists) throws IOException {
-        FileOutputStream fileToModif = null;
-        try {
-            fileToModif = new FileOutputStream(file, exists);
-            fileToModif.write(fileData);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (fileToModif != null) fileToModif.close();
+
+    // Construction du header pour les méthodes get et head. Renvoie le fichier à afficher dans le cas de la méthode get
+    public File buildHeaderGetandPost(String fileName, PrintWriter out) throws IOException {
+        // si pas de fichier spécifié, on affiche index.html
+        if (fileName.equals("/") || fileName.equals("")) {
+            fileName = INIT_DIR + "index.html";
+        } else {
+            fileName = INIT_DIR + fileName;
         }
-    }
-    
-    public List<String> getHeaderFromClient(BufferedInputStream in) throws IOException {
-        int charRead = in.read();
-        List<String> listeHeader = new ArrayList<>();
-        StringBuilder header = new StringBuilder();
-        while (charRead != -1) {
-            header.append((char) charRead);
-            if (header.toString().endsWith("\r\n")) {
-                if (header.toString().equals("\r\n")) {
-                    break;
-                } else {
-                    listeHeader.add(header.substring(0, header.toString().lastIndexOf("\r\n")));
-                    header = new StringBuilder();
-                }
-            }
-            charRead = in.read();
+        File file = new File(fileName);
+        String codeStatus = "OK 200", extension = "";
+        // on vérifie que le fichier existe, sinon message d'erreur
+        if (!file.exists()) {
+            codeStatus = "404 not found";
+            fileName = INIT_DIR + "errorNonTrouve.html";
+            file = new File(fileName);
         }
-        System.out.println(listeHeader);
-        return listeHeader;
+        int fileLength = (int) file.length();
+        extension = null;
+        int extensionPos = fileName.lastIndexOf('.');
+        if (extensionPos > 0) {
+            extension = fileName.substring(extensionPos + 1);
+        } else { // pas sûr de vraiment pouvoir passer là
+            codeStatus = "404 not found";
+        }
+
+        String content = getTypeFromExtension(extension);
+        // on affiche le headerr avec les infos récolotées
+        printHeader(content, codeStatus, fileLength, out, fileName);
+
+        return file;
     }
 
 
@@ -353,6 +278,85 @@ public class WebServer {
 
         }
         return file;
+    }
+
+    // affiche le header dans le print writer out
+    public void printHeader(String content, String status, int length, PrintWriter out, String fileName) throws IOException {
+        String header = "HTTP/1.1 " + status + "\r\n";
+        header += "Server: Java HTTP Server from Capucine and Arthur : 1.0" + "\r\n";
+        header += "Date: " + new Date() + "\r\n";
+        if (!content.equals(""))
+            header += "Content-Type: " + content + "\r\n";
+        if (length != -1)
+            header += "Content-length: " + length + "\r\n";
+        if (!fileName.equals(""))
+            header += "Content-name: " + fileName + "\r\n";
+        out.println(header);
+        out.flush();
+    }
+
+    // donne le type du fichier en fonction de son extension
+    public String getTypeFromExtension(String extension) {
+        String type = "";
+        switch (extension) {
+            case "jpg":
+            case "jpeg":
+                type = "image/jpeg";
+                break;
+            case "gif":
+                type = "image/gif";
+                break;
+            case "png":
+                type = "image/png";
+                break;
+            case "mp4":
+                type = "video/mp4";
+                break;
+            case "docx":
+                type = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                break;
+            case "pdf":
+                type = "application/pdf";
+                break;
+            case "md":
+                type = "text/markdown";
+                break;
+            case "html":
+                type = "text/html";
+                break;
+            default:
+                type = "text/plain";
+                break;
+        }
+        return type;
+    }
+
+
+    // on lit le fichier en paramètre et on le retourne sous forme de tableau de byte
+    public byte[] readData(File file) throws IOException {
+        int lengthFile = (int) file.length();
+        FileInputStream dataStream = null;
+        byte[] dataArray = new byte[lengthFile];
+        try {
+            dataStream = new FileInputStream(file);
+            dataStream.read(dataArray);
+        } finally {
+            if (dataStream != null) dataStream.close();
+        }
+        return dataArray;
+    }
+
+    // on écrit les données récupérées au format byte pour les insérer dans le fichier en paramètre
+    private void writeData(File file, byte[] fileData, boolean exists) throws IOException {
+        FileOutputStream fileToModif = null;
+        try {
+            fileToModif = new FileOutputStream(file, exists);
+            fileToModif.write(fileData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileToModif != null) fileToModif.close();
+        }
     }
 
 
